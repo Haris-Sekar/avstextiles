a7b61d45bff308faed132957e60574c841d49be2
Haris S <sharis14003@gmail.com>
2023-01-24 21:14:48 +0530

    Nodejs baseurl change in reactjs

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseUrl = axios.create({
-    baseURL:"http://127.0.0.1:5000"
+    baseURL:"https://avstextiles.onrender.com"
 });
 baseUrl.interceptors.request.use((req) => {
     if (localStorage.getItem('token')) {
